6a26b83ea66a79fe808fb3d7c47bb2d16a2cf958
Ahmed Khalaf <pxlshpr@gmail.com>
2022-05-25 10:33:45 +0500

    tag increment

--- Sources/NutritionLabelClassifier/Structs/Output.swift
+++ Sources/NutritionLabelClassifier/Structs/Output.swift
@@ -1,14 +1,68 @@
 import Foundation
 
-public enum ContainerName {
-    case container
-    case package
-    case unknown
+public struct Output {
+    public let serving: Serving?
+    public let nutrients: Nutrients
+    public let primaryColumnIndex: Int
 }
 
-public struct Output {
+extension Output {
+    //MARK: Serving
+    public struct Serving {
+        public let amount: Amount?
+        public let perContainer: PerContainer?
 
+        public struct Amount {
+            public let value: DoubleWithId
+            public let unit: UnitWithId?
+            public let unitSize: StringWithId?
+            public let equivalentSize: EquivalentSize?
+
+            public struct EquivalentSize {
+                public let name: StringWithId
+                public let value: DoubleWithId
+            }
+        }
+        
+        public struct PerContainer {
+            public let value: DoubleWithId
+            public let name: ContainerNameWithId?
+
+            public struct ContainerNameWithId {
+                public let containerName: ContainerName
+                public let id: UUID
+            }
+        }
+    }
     
+    //MARK: Nutrients
+    public struct Nutrients {
+        public let columnHeader1: ColumnHeaderWithId?
+        public let columnHeader2: ColumnHeaderWithId?
+        public let rows: [NutrientRow]
+        
+        public struct ColumnHeaderWithId {
+            public let columnHeader: ColumnHeader
+            public let id: UUID
+        }
+        
+        public struct NutrientRow {
+            public let attribute: AttributeWithId
+            public let value1: ValueWithId?
+            public let value2: ValueWithId?
+
+            public struct AttributeWithId {
+                public let attribute: Attribute
+                public let id: UUID
+            }
+            
+            public struct ValueWithId {
+                public let value: Value
+            }
+        }
+    }
+    
+    //MARK: Containers
     public struct DoubleWithId {
         public let double: Double
         public let id: UUID
@@ -23,64 +77,10 @@ public struct Output {
         public let string: String
         public let id: UUID
     }
+}
 
-    public struct Serving {
-        public struct Amount {
-
-            public struct EquivalentSize {
-                public let name: StringWithId
-                public let value: DoubleWithId
-            }
-            
-            public let value: DoubleWithId
-            public let unit: UnitWithId?
-            public let unitSize: StringWithId?
-            public let equivalentSize: EquivalentSize?
-        }
-        
-        public struct PerContainer {
-            
-            public struct ContainerNameWithId {
-                public let containerName: ContainerName
-                public let id: UUID
-            }
-            
-            public let value: DoubleWithId
-            public let name: ContainerNameWithId?
-        }
-        
-        public let amount: Amount?
-        public let perContainer: PerContainer?
-    }
-    
-    public struct Nutrients {
-        public struct ColumnHeaderWithId {
-            public let columnHeader: ColumnHeader
-            public let id: UUID
-        }
-        
-        public struct NutrientRow {
-            
-            public struct AttributeWithId {
-                public let attribute: Attribute
-                public let id: UUID
-            }
-            
-            public struct ValueWithId {
-                public let value: Value
-            }
-            
-            public let attribute: AttributeWithId
-            public let value1: ValueWithId?
-            public let value2: ValueWithId?
-        }
-        
-        public let columnHeader1: ColumnHeaderWithId?
-        public let columnHeader2: ColumnHeaderWithId?
-        public let rows: [NutrientRow]
-    }
-    
-    public let serving: Serving?
-    public let nutrients: Nutrients
-    public let primaryColumnIndex: Int
+public enum ContainerName {
+    case container
+    case package
+    case unknown
 }
