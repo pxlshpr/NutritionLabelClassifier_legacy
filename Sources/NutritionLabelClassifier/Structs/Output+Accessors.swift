e6932bff191374824268643c7e06045359ef7a8a
Ahmed Khalaf <pxlshpr@gmail.com>
2022-05-28 12:55:41 +0500

    tag increment

---
+++ Sources/NutritionLabelClassifier/Structs/Output+Accessors.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+public extension Output.Serving {
+    var amount: Double? { identifiableAmount?.double }
+    var unit: NutritionUnit? { identifiableUnit?.nutritionUnit }
+    var unitSizeName: String? { identifiableUnitSizeName?.string }
+    
+    var amountId: UUID? { identifiableAmount?.id }
+    var unitId: UUID? { identifiableUnit?.id }
+    var unitSizeNameId: UUID? { identifiableUnitSizeName?.id }
+}
+
+public extension Output.Serving.EquivalentSize {
+    var amount: Double { identifiableAmount.double }
+    var unit: NutritionUnit? { identifiableUnit?.nutritionUnit }
+    var sizeName: String? { identifiableUnitSizeName?.string }
+    
+    var amountId: UUID { identifiableAmount.id }
+    var unitId: UUID? { identifiableUnit?.id }
+    var sizeNameId: UUID? { identifiableUnitSizeName?.id }
+}
+
+public extension Output.Serving.PerContainer {
+    var amount: Double { identifiableAmount.double }
+    var name: String? { identifiableName?.string }
+//    var containerName: ContainerName? { identifiableContainerName?.containerName }
+    
+    var amountId: UUID { identifiableAmount.id }
+    var nameId: UUID? { identifiableName?.id }
+//    var containerNameId: UUID? { identifiableContainerName?.id }
+}
+
+public extension Output.Nutrients {
+    var columnHeader1: ColumnHeader? { identifiableColumnHeader1?.columnHeader }
+    var columnHeader2: ColumnHeader? { identifiableColumnHeader2?.columnHeader }
+
+    var columnHeader1Id: UUID? { identifiableColumnHeader1?.id }
+    var columnHeader2Id: UUID? { identifiableColumnHeader2?.id }
+}
+
+public extension Output.Nutrients.Row {
+    var attribute: Attribute { identifiableAttribute.attribute }
+    var value1: Value? { identifiableValue1?.value }
+    var value2: Value? { identifiableValue2?.value }
+    
+    var attributeId: UUID { identifiableAttribute.id }
+    var value1Id: UUID? { identifiableValue1?.id }
+    var value2Id: UUID? { identifiableValue2?.id }
+}
---
+++ Sources/NutritionLabelClassifier/Structs/Output+Helpers.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+extension Output {
+    var containsServingAttributes: Bool {
+        guard let serving = serving else { return false }
+        return serving.amount != nil
+        || serving.unit != nil
+        || serving.unitSizeName != nil
+        || serving.equivalentSize != nil
+        || serving.perContainer != nil
+    }
+    
+    func containsAttribute(_ attribute: Attribute) -> Bool {
+        switch attribute {
+        case .nutritionFacts:
+            return false
+        case .servingAmount:
+            return serving?.amount != nil
+        case .servingUnit:
+            return serving?.unit != nil
+        case .servingUnitSize:
+            return serving?.unitSizeName != nil
+        case .servingEquivalentAmount:
+            return serving?.equivalentSize != nil
+        case .servingEquivalentUnit:
+            return serving?.equivalentSize?.unit != nil
+        case .servingEquivalentUnitSize:
+            return serving?.equivalentSize?.sizeName != nil
+        case .servingsPerContainerAmount:
+            return serving?.perContainer != nil
+        case .servingsPerContainerName:
+            return serving?.perContainer?.name != nil
+        case .nutrientsColumnHeader1:
+            return nutrients.identifiableColumnHeader1 != nil
+        case .nutrientsColumnHeader2:
+            return nutrients.identifiableColumnHeader2 != nil
+        default:
+            return nutrients.rows.contains(where: { $0.attribute == attribute })
+        }
+    }
+}
