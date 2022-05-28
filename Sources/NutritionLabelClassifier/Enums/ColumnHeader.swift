57d966b2633bab25067e20e2fdd13684ca1bab5e
Ahmed Khalaf <pxlshpr@gmail.com>
2022-05-28 13:24:29 +0500

    tag increment

--- Sources/NutritionLabelClassifier/Enums/ColumnHeader.swift
+++ Sources/NutritionLabelClassifier/Enums/ColumnHeader.swift
@@ -4,3 +4,14 @@ public enum ColumnHeaderType: Int, CaseIterable {
     case per100g = 1
     case perCustomSize = 1000
 }
+
+extension ColumnHeaderType: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .per100g:
+            return "Per 100g"
+        case .perCustomSize:
+            return "Per Custom Size"
+        }
+    }
+}
