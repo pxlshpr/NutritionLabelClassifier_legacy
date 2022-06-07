bb8312c7c7196dc9ddc6648af10b6598661ddd07
Ahmed Khalaf <pxlshpr@gmail.com>
2022-06-07 12:14:38 +0500

    tag increment

--- Sources/NutritionLabelClassifier/NutritionLabelClassifier.swift
+++ Sources/NutritionLabelClassifier/NutritionLabelClassifier.swift
@@ -2,7 +2,7 @@ import Foundation
 import VisionSugar
 import TabularData
 
-public let NutritionLabelClassifierVersion = "0.0.143"
+public let NutritionLabelClassifierVersion = "0.0.148"
 
 //TODO: Rename this to
 /// `NutritionLabelRecognizer`
